0d2d74c4513cdcbfaa5f6f1b9ba09c0050840c44
Derek Hamel <dbhamel@asu.edu>
2015-07-12 01:17:31 -0700

    first

---
+++ src/application/Main.java
@@ -0,0 +1,90 @@
+package application;
+	
+import java.io.InputStream;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	private final double MINIMUM_WINDOW_WIDTH = 1280;
+    private final double MINIMUM_WINDOW_HEIGHT = 840;
+    private Scene scene;
+    private Stage stage;
+    
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage = primaryStage;
+			primaryStage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+			primaryStage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+
+                }
+            });
+			SceneController ctr = (SceneController) replaceSceneContent("scene.fxml", null);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public Node replaceSceneContent(String fxml, Class<? extends AnchorPane> cls) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        if (cls == null) {
+            //System.out.println("controller was set by FXML");
+        } else {
+            loader.setController(cls.getConstructor().newInstance()); //set controller manually
+        }
+        InputStream in = Main.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(Main.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        }
+
+        // Store the stage width and height in case the user has resized the window
+        double stageWidth = stage.getWidth();
+        if (!Double.isNaN(stageWidth)) {
+            stageWidth -= (stage.getWidth() - stage.getScene().getWidth());
+        }
+
+        double stageHeight = stage.getHeight();
+        if (!Double.isNaN(stageHeight)) {
+            stageHeight -= (stage.getHeight() - stage.getScene().getHeight());
+        }
+
+        Scene scene = new Scene(page);
+        if (!Double.isNaN(stageWidth)) {
+            page.setPrefWidth(stageWidth);
+        }
+        if (!Double.isNaN(stageHeight)) {
+            page.setPrefHeight(stageHeight);
+        }
+
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Node) loader.getController();
+    }
+}
---
+++ src/application/SceneController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+public class SceneController extends AnchorPane{
+	@FXML
+	private ToggleGroup solarone;
+	@FXML
+	private ToggleGroup solartwo;
+	@FXML
+	private ToggleGroup solarthree;
+
+}
